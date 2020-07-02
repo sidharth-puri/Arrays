46f25599e6b7889139af6682843605c89eb0f162
Sidharth Puri <52946626+sidharth-puri@users.noreply.github.com>
2020-07-02 22:38:37 +0530

    Create Move_0&1_poles_apart.cpp

---
+++ Move_0&1_poles_apart.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int i,j=0,k=n-1;
+    for(i=0;i<n;i++) {
+        if(s[i]=='0') {
+            swap(s[j],s[i]);
+            j++;
+        }
+    }
+   // cout<<s<<endl;
+    for(i=n-1;i>=0;i--) {
+        if(s[i]=='1') {
+            swap(s[i],s[k]);
+            k--;
+        }
+    }
+    cout<<s;
+}
